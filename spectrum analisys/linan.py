5136eff59e8c433fd4d69e8d117c30221a0eabaa
Ilnur Nurmukhametov <ilnur852@gmail.com>
2023-06-01 00:15:11 +0300

    truing and crying

--- spectrum analisys/linan.py
+++ spectrum analisys/linan.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy import linalg as la
 
 N = 32
-p = 1
+p = 4
 sRate = 128
 
 ovs = sRate * N
@@ -12,10 +12,12 @@ pi = np.pi
 
 #generate sequence
 t = np.linspace(0, 2*pi, num=N)
-x =  np.sin(2*np.pi * t) + np.random.random(N) * 0.3
+x =  np.sin(3*t) + np.random.random(N) * 0.1
 
 plt.plot(x)
 plt.show()
+
+Rxest = (1/len(x))*np.outer(np.asarray(x), np.asarray(x))
 
 #find autocorrelations
 conj = np.conj(x)
@@ -26,7 +28,7 @@ acf =  np.convolve(x, flip) #autocorrelation function - convotution of time reve
 Rxx = la.toeplitz(acf)
 
 #eigendecomposition of Rxx
-v, d = la.eig(Rxx) # v- eigenvalues , d - corresponding eigenvectors 
+v, d = la.eig(Rxest) # v- eigenvalues , d - corresponding eigenvectors 
 
 indexes = np.argsort(v)[::-1]
 sort_eigvals = v[indexes] # sorting eigenvalues from greatest to least (retr)
@@ -42,12 +44,12 @@ print("length noise eigenvector", len(noise_eigvects), len(noise_eigvects[0]))
 
 #perform MUSIC frequency estimation
 Pmu = []
-for k in range(0, ovs + 1):
+for k in range(0, len(noise_eigvects[0])):
     sum1 = 0
     sum2 = 0
     freq_vector = np.zeros(len(noise_eigvects[0]), dtype=np.complex_)
 
-    f = float(k)/ovs
+    f = float(k)/len(noise_eigvects[0])
 
     for i in range(0, len(noise_eigvects[0])):
         freq_vector[i] = np.conjugate(complex(np.cos(2 * pi* i * f), np.sin(2 * pi * i *f))) # steering vector e definition
@@ -56,7 +58,7 @@ for k in range(0, ovs + 1):
     for u in range(0, len(noise_eigvects)):
         sum1 += (abs(np.dot(np.asarray(freq_vector).transpose(), np.asarray(noise_eigvects[u]))))**2 # |e^H * vk|^2
 
-    #print(f, 1/sum)
+    print(f, 1/sum1)
     Pmu = np.append(Pmu, 1/sum1)
 
     #sum2 += np.fft.fft(noise_eigvects[:, k], 1024)
