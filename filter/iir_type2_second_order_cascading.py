30ab1d7f4291d1ec2935221d30350fa6357b373e
Ilnur Nurmikhametov <ilnur852@gmail.com>
2023-12-07 10:10:18 +0300

    remove unnecessaty files

--- filter/iir_type2_second_order_cascading.py
+++ filter/iir_type2_second_order_cascading.py
@@ -9,8 +9,8 @@ def t2_iir(x ,a, b, order):
     yk = 0
     w = 0
     for i in range(len(x)): #iteratevely output filter stages
-        w = -np.dot(a[1:], xz[1:][::-1]) + x[i]
-        yk= np.dot(b, xz[::-1])
+        w = np.dot(-a[1:], xz[1:][::-1]) + x[i]
+        yk= np.dot(b, xz)
         xz = np.append(xz[1:], w)
         y = np.append(y , yk)
     return y
@@ -21,7 +21,6 @@ def bin_repr_file_save(data, file, width):
         fdat = np.floor(data[i]).astype(int)
         file.write((np.binary_repr(fdat, width=width) + "\n"))
 
-
 if __name__=="__main__":
     with np.load("filtcoef.npz") as data:
         d = data['ba']
@@ -29,23 +28,31 @@ if __name__=="__main__":
     b = d[: ,0]
     a = a.astype(np.float32)
     b = b.astype(np.float32)
+
     print(a)
     print(b)
-    x = np.linspace(0, 5, 1024)
-    s  = np.sin(np.exp(x))
+
+    t = np.linspace(0, 625/2, 625*128)
+    s = 2**15*signal.chirp(t, f0=.01, f1=2, t1=625/16, method='linear')
     outfile1 = open("sw.txt", "w")
-    bin_repr_file_save(s*2**9, outfile1, 10)
+    bin_repr_file_save(s, outfile1, 16)
     outfile1.close()
-    
-    fo = t2_iir(s , a, b, len(b)-1)
-    fo_s = signal.lfilter(b,a,s)
-    plt.subplot(311)
-    plt.title('input sequence')
+    w ,h  = signal.freqz(b, a)
+
+    #fo = t2_iir(s , a, b, len(b)-1)
+    fo = signal.lfilter(b, a, s)
+
+    plt.plot(w, 20 * np.log10(abs(h)), 'b')
+    plt.ylabel('Amplitude [dB]', color='b')
+    plt.xlabel('Frequency [rad/sample]')
+    plt.grid()
+
+
+    plt.figure()
+    plt.subplot(211)
     plt.plot(s)
-    plt.subplot(312)
+    plt.xlabel('t (sec)')
+    plt.subplot(212)
     plt.title('filtered w/ custom implementation')
     plt.plot(fo)
-    plt.subplot(313)
-    plt.title('filtered using scipy lfilt')
-    plt.plot(fo_s)
     plt.show()
